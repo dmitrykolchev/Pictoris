ae10f99015a43ffde347f79a40a597d68d7f72a7
Dmitry Kolchev <dmitrykolchev@msn.com>
2025-07-13 16:44:31 +0300

    generate Base.cs

--- Managed.Win32/Win32/Base/Base.cs
+++ Managed.Win32/Win32/Base/Base.cs
@@ -1575,4 +1575,391 @@ namespace Managed.Win32.Base
             }
         }
     }
+
+    public static unsafe partial class Methods
+    {
+        [NativeTypeName("#define HFILE_ERROR ((HFILE)-1)")]
+        public const int HFILE_ERROR = ((int)(-1));
+
+        [NativeTypeName("#define APP_LOCAL_DEVICE_ID_SIZE 32")]
+        public const int APP_LOCAL_DEVICE_ID_SIZE = 32;
+
+        [NativeTypeName("#define DM_UPDATE 1")]
+        public const int DM_UPDATE = 1;
+
+        [NativeTypeName("#define DM_COPY 2")]
+        public const int DM_COPY = 2;
+
+        [NativeTypeName("#define DM_PROMPT 4")]
+        public const int DM_PROMPT = 4;
+
+        [NativeTypeName("#define DM_MODIFY 8")]
+        public const int DM_MODIFY = 8;
+
+        [NativeTypeName("#define DM_IN_BUFFER DM_MODIFY")]
+        public const int DM_IN_BUFFER = 8;
+
+        [NativeTypeName("#define DM_IN_PROMPT DM_PROMPT")]
+        public const int DM_IN_PROMPT = 4;
+
+        [NativeTypeName("#define DM_OUT_BUFFER DM_COPY")]
+        public const int DM_OUT_BUFFER = 2;
+
+        [NativeTypeName("#define DM_OUT_DEFAULT DM_UPDATE")]
+        public const int DM_OUT_DEFAULT = 1;
+
+        [NativeTypeName("#define DC_FIELDS 1")]
+        public const int DC_FIELDS = 1;
+
+        [NativeTypeName("#define DC_PAPERS 2")]
+        public const int DC_PAPERS = 2;
+
+        [NativeTypeName("#define DC_PAPERSIZE 3")]
+        public const int DC_PAPERSIZE = 3;
+
+        [NativeTypeName("#define DC_MINEXTENT 4")]
+        public const int DC_MINEXTENT = 4;
+
+        [NativeTypeName("#define DC_MAXEXTENT 5")]
+        public const int DC_MAXEXTENT = 5;
+
+        [NativeTypeName("#define DC_BINS 6")]
+        public const int DC_BINS = 6;
+
+        [NativeTypeName("#define DC_DUPLEX 7")]
+        public const int DC_DUPLEX = 7;
+
+        [NativeTypeName("#define DC_SIZE 8")]
+        public const int DC_SIZE = 8;
+
+        [NativeTypeName("#define DC_EXTRA 9")]
+        public const int DC_EXTRA = 9;
+
+        [NativeTypeName("#define DC_VERSION 10")]
+        public const int DC_VERSION = 10;
+
+        [NativeTypeName("#define DC_DRIVER 11")]
+        public const int DC_DRIVER = 11;
+
+        [NativeTypeName("#define DC_BINNAMES 12")]
+        public const int DC_BINNAMES = 12;
+
+        [NativeTypeName("#define DC_ENUMRESOLUTIONS 13")]
+        public const int DC_ENUMRESOLUTIONS = 13;
+
+        [NativeTypeName("#define DC_FILEDEPENDENCIES 14")]
+        public const int DC_FILEDEPENDENCIES = 14;
+
+        [NativeTypeName("#define DC_TRUETYPE 15")]
+        public const int DC_TRUETYPE = 15;
+
+        [NativeTypeName("#define DC_PAPERNAMES 16")]
+        public const int DC_PAPERNAMES = 16;
+
+        [NativeTypeName("#define DC_ORIENTATION 17")]
+        public const int DC_ORIENTATION = 17;
+
+        [NativeTypeName("#define DC_COPIES 18")]
+        public const int DC_COPIES = 18;
+
+        [NativeTypeName("#define DPI_AWARENESS_CONTEXT_UNAWARE ((DPI_AWARENESS_CONTEXT)-1)")]
+        public static DPI_AWARENESS_CONTEXT* DPI_AWARENESS_CONTEXT_UNAWARE => unchecked((DPI_AWARENESS_CONTEXT*)(-1));
+
+        [NativeTypeName("#define DPI_AWARENESS_CONTEXT_SYSTEM_AWARE ((DPI_AWARENESS_CONTEXT)-2)")]
+        public static DPI_AWARENESS_CONTEXT* DPI_AWARENESS_CONTEXT_SYSTEM_AWARE => unchecked((DPI_AWARENESS_CONTEXT*)(-2));
+
+        [NativeTypeName("#define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE ((DPI_AWARENESS_CONTEXT)-3)")]
+        public static DPI_AWARENESS_CONTEXT* DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE => unchecked((DPI_AWARENESS_CONTEXT*)(-3));
+
+        [NativeTypeName("#define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 ((DPI_AWARENESS_CONTEXT)-4)")]
+        public static DPI_AWARENESS_CONTEXT* DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 => unchecked((DPI_AWARENESS_CONTEXT*)(-4));
+
+        [NativeTypeName("#define DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED ((DPI_AWARENESS_CONTEXT)-5)")]
+        public static DPI_AWARENESS_CONTEXT* DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED => unchecked((DPI_AWARENESS_CONTEXT*)(-5));
+
+        [NativeTypeName("#define FIND_FIRST_EX_CASE_SENSITIVE 0x00000001")]
+        public const int FIND_FIRST_EX_CASE_SENSITIVE = 0x00000001;
+
+        [NativeTypeName("#define FIND_FIRST_EX_LARGE_FETCH 0x00000002")]
+        public const int FIND_FIRST_EX_LARGE_FETCH = 0x00000002;
+
+        [NativeTypeName("#define FIND_FIRST_EX_ON_DISK_ENTRIES_ONLY 0x00000004")]
+        public const int FIND_FIRST_EX_ON_DISK_ENTRIES_ONLY = 0x00000004;
+
+        [NativeTypeName("#define LOCKFILE_FAIL_IMMEDIATELY 0x00000001")]
+        public const int LOCKFILE_FAIL_IMMEDIATELY = 0x00000001;
+
+        [NativeTypeName("#define LOCKFILE_EXCLUSIVE_LOCK 0x00000002")]
+        public const int LOCKFILE_EXCLUSIVE_LOCK = 0x00000002;
+
+        [NativeTypeName("#define PROCESS_HEAP_REGION 0x0001")]
+        public const int PROCESS_HEAP_REGION = 0x0001;
+
+        [NativeTypeName("#define PROCESS_HEAP_UNCOMMITTED_RANGE 0x0002")]
+        public const int PROCESS_HEAP_UNCOMMITTED_RANGE = 0x0002;
+
+        [NativeTypeName("#define PROCESS_HEAP_ENTRY_BUSY 0x0004")]
+        public const int PROCESS_HEAP_ENTRY_BUSY = 0x0004;
+
+        [NativeTypeName("#define PROCESS_HEAP_SEG_ALLOC 0x0008")]
+        public const int PROCESS_HEAP_SEG_ALLOC = 0x0008;
+
+        [NativeTypeName("#define PROCESS_HEAP_ENTRY_MOVEABLE 0x0010")]
+        public const int PROCESS_HEAP_ENTRY_MOVEABLE = 0x0010;
+
+        [NativeTypeName("#define PROCESS_HEAP_ENTRY_DDESHARE 0x0020")]
+        public const int PROCESS_HEAP_ENTRY_DDESHARE = 0x0020;
+
+        [NativeTypeName("#define EXCEPTION_DEBUG_EVENT 1")]
+        public const int EXCEPTION_DEBUG_EVENT = 1;
+
+        [NativeTypeName("#define CREATE_THREAD_DEBUG_EVENT 2")]
+        public const int CREATE_THREAD_DEBUG_EVENT = 2;
+
+        [NativeTypeName("#define CREATE_PROCESS_DEBUG_EVENT 3")]
+        public const int CREATE_PROCESS_DEBUG_EVENT = 3;
+
+        [NativeTypeName("#define EXIT_THREAD_DEBUG_EVENT 4")]
+        public const int EXIT_THREAD_DEBUG_EVENT = 4;
+
+        [NativeTypeName("#define EXIT_PROCESS_DEBUG_EVENT 5")]
+        public const int EXIT_PROCESS_DEBUG_EVENT = 5;
+
+        [NativeTypeName("#define LOAD_DLL_DEBUG_EVENT 6")]
+        public const int LOAD_DLL_DEBUG_EVENT = 6;
+
+        [NativeTypeName("#define UNLOAD_DLL_DEBUG_EVENT 7")]
+        public const int UNLOAD_DLL_DEBUG_EVENT = 7;
+
+        [NativeTypeName("#define OUTPUT_DEBUG_STRING_EVENT 8")]
+        public const int OUTPUT_DEBUG_STRING_EVENT = 8;
+
+        [NativeTypeName("#define RIP_EVENT 9")]
+        public const int RIP_EVENT = 9;
+
+        [NativeTypeName("#define STILL_ACTIVE STATUS_PENDING")]
+        public const uint STILL_ACTIVE = ((uint)(0x00000103));
+
+        [NativeTypeName("#define EXCEPTION_ACCESS_VIOLATION STATUS_ACCESS_VIOLATION")]
+        public const uint EXCEPTION_ACCESS_VIOLATION = ((uint)(0xC0000005));
+
+        [NativeTypeName("#define EXCEPTION_DATATYPE_MISALIGNMENT STATUS_DATATYPE_MISALIGNMENT")]
+        public const uint EXCEPTION_DATATYPE_MISALIGNMENT = ((uint)(0x80000002));
+
+        [NativeTypeName("#define EXCEPTION_BREAKPOINT STATUS_BREAKPOINT")]
+        public const uint EXCEPTION_BREAKPOINT = ((uint)(0x80000003));
+
+        [NativeTypeName("#define EXCEPTION_SINGLE_STEP STATUS_SINGLE_STEP")]
+        public const uint EXCEPTION_SINGLE_STEP = ((uint)(0x80000004));
+
+        [NativeTypeName("#define EXCEPTION_ARRAY_BOUNDS_EXCEEDED STATUS_ARRAY_BOUNDS_EXCEEDED")]
+        public const uint EXCEPTION_ARRAY_BOUNDS_EXCEEDED = ((uint)(0xC000008C));
+
+        [NativeTypeName("#define EXCEPTION_FLT_DENORMAL_OPERAND STATUS_FLOAT_DENORMAL_OPERAND")]
+        public const uint EXCEPTION_FLT_DENORMAL_OPERAND = ((uint)(0xC000008D));
+
+        [NativeTypeName("#define EXCEPTION_FLT_DIVIDE_BY_ZERO STATUS_FLOAT_DIVIDE_BY_ZERO")]
+        public const uint EXCEPTION_FLT_DIVIDE_BY_ZERO = ((uint)(0xC000008E));
+
+        [NativeTypeName("#define EXCEPTION_FLT_INEXACT_RESULT STATUS_FLOAT_INEXACT_RESULT")]
+        public const uint EXCEPTION_FLT_INEXACT_RESULT = ((uint)(0xC000008F));
+
+        [NativeTypeName("#define EXCEPTION_FLT_INVALID_OPERATION STATUS_FLOAT_INVALID_OPERATION")]
+        public const uint EXCEPTION_FLT_INVALID_OPERATION = ((uint)(0xC0000090));
+
+        [NativeTypeName("#define EXCEPTION_FLT_OVERFLOW STATUS_FLOAT_OVERFLOW")]
+        public const uint EXCEPTION_FLT_OVERFLOW = ((uint)(0xC0000091));
+
+        [NativeTypeName("#define EXCEPTION_FLT_STACK_CHECK STATUS_FLOAT_STACK_CHECK")]
+        public const uint EXCEPTION_FLT_STACK_CHECK = ((uint)(0xC0000092));
+
+        [NativeTypeName("#define EXCEPTION_FLT_UNDERFLOW STATUS_FLOAT_UNDERFLOW")]
+        public const uint EXCEPTION_FLT_UNDERFLOW = ((uint)(0xC0000093));
+
+        [NativeTypeName("#define EXCEPTION_INT_DIVIDE_BY_ZERO STATUS_INTEGER_DIVIDE_BY_ZERO")]
+        public const uint EXCEPTION_INT_DIVIDE_BY_ZERO = ((uint)(0xC0000094));
+
+        [NativeTypeName("#define EXCEPTION_INT_OVERFLOW STATUS_INTEGER_OVERFLOW")]
+        public const uint EXCEPTION_INT_OVERFLOW = ((uint)(0xC0000095));
+
+        [NativeTypeName("#define EXCEPTION_PRIV_INSTRUCTION STATUS_PRIVILEGED_INSTRUCTION")]
+        public const uint EXCEPTION_PRIV_INSTRUCTION = ((uint)(0xC0000096));
+
+        [NativeTypeName("#define EXCEPTION_IN_PAGE_ERROR STATUS_IN_PAGE_ERROR")]
+        public const uint EXCEPTION_IN_PAGE_ERROR = ((uint)(0xC0000006));
+
+        [NativeTypeName("#define EXCEPTION_ILLEGAL_INSTRUCTION STATUS_ILLEGAL_INSTRUCTION")]
+        public const uint EXCEPTION_ILLEGAL_INSTRUCTION = ((uint)(0xC000001D));
+
+        [NativeTypeName("#define EXCEPTION_NONCONTINUABLE_EXCEPTION STATUS_NONCONTINUABLE_EXCEPTION")]
+        public const uint EXCEPTION_NONCONTINUABLE_EXCEPTION = ((uint)(0xC0000025));
+
+        [NativeTypeName("#define EXCEPTION_STACK_OVERFLOW STATUS_STACK_OVERFLOW")]
+        public const uint EXCEPTION_STACK_OVERFLOW = ((uint)(0xC00000FD));
+
+        [NativeTypeName("#define EXCEPTION_INVALID_DISPOSITION STATUS_INVALID_DISPOSITION")]
+        public const uint EXCEPTION_INVALID_DISPOSITION = ((uint)(0xC0000026));
+
+        [NativeTypeName("#define EXCEPTION_GUARD_PAGE STATUS_GUARD_PAGE_VIOLATION")]
+        public const uint EXCEPTION_GUARD_PAGE = ((uint)(0x80000001));
+
+        [NativeTypeName("#define EXCEPTION_INVALID_HANDLE STATUS_INVALID_HANDLE")]
+        public const uint EXCEPTION_INVALID_HANDLE = ((uint)(0xC0000008));
+
+        [NativeTypeName("#define CONTROL_C_EXIT STATUS_CONTROL_C_EXIT")]
+        public const uint CONTROL_C_EXIT = ((uint)(0xC000013A));
+
+        [NativeTypeName("#define LMEM_FIXED 0x0000")]
+        public const int LMEM_FIXED = 0x0000;
+
+        [NativeTypeName("#define LMEM_MOVEABLE 0x0002")]
+        public const int LMEM_MOVEABLE = 0x0002;
+
+        [NativeTypeName("#define LMEM_NOCOMPACT 0x0010")]
+        public const int LMEM_NOCOMPACT = 0x0010;
+
+        [NativeTypeName("#define LMEM_NODISCARD 0x0020")]
+        public const int LMEM_NODISCARD = 0x0020;
+
+        [NativeTypeName("#define LMEM_ZEROINIT 0x0040")]
+        public const int LMEM_ZEROINIT = 0x0040;
+
+        [NativeTypeName("#define LMEM_MODIFY 0x0080")]
+        public const int LMEM_MODIFY = 0x0080;
+
+        [NativeTypeName("#define LMEM_DISCARDABLE 0x0F00")]
+        public const int LMEM_DISCARDABLE = 0x0F00;
+
+        [NativeTypeName("#define LMEM_VALID_FLAGS 0x0F72")]
+        public const int LMEM_VALID_FLAGS = 0x0F72;
+
+        [NativeTypeName("#define LMEM_INVALID_HANDLE 0x8000")]
+        public const int LMEM_INVALID_HANDLE = 0x8000;
+
+        [NativeTypeName("#define LHND (LMEM_MOVEABLE | LMEM_ZEROINIT)")]
+        public const int LHND = (0x0002 | 0x0040);
+
+        [NativeTypeName("#define LPTR (LMEM_FIXED | LMEM_ZEROINIT)")]
+        public const int LPTR = (0x0000 | 0x0040);
+
+        [NativeTypeName("#define NONZEROLHND (LMEM_MOVEABLE)")]
+        public const int NONZEROLHND = (0x0002);
+
+        [NativeTypeName("#define NONZEROLPTR (LMEM_FIXED)")]
+        public const int NONZEROLPTR = (0x0000);
+
+        [NativeTypeName("#define LMEM_DISCARDED 0x4000")]
+        public const int LMEM_DISCARDED = 0x4000;
+
+        [NativeTypeName("#define LMEM_LOCKCOUNT 0x00FF")]
+        public const int LMEM_LOCKCOUNT = 0x00FF;
+
+        [NativeTypeName("#define NUMA_NO_PREFERRED_NODE ((DWORD) -1)")]
+        public const uint NUMA_NO_PREFERRED_NODE = unchecked((uint)(-1));
+
+        [NativeTypeName("#define __REQUIRED_RPCNDR_H_VERSION__ 501")]
+        public const int __REQUIRED_RPCNDR_H_VERSION__ = 501;
+
+        [NativeTypeName("#define __REQUIRED_RPCSAL_H_VERSION__ 100")]
+        public const int __REQUIRED_RPCSAL_H_VERSION__ = 100;
+
+        [NativeTypeName("#define ROTREGFLAGS_ALLOWANYCLIENT 0x1")]
+        public const int ROTREGFLAGS_ALLOWANYCLIENT = 0x1;
+
+        [NativeTypeName("#define APPIDREGFLAGS_ACTIVATE_IUSERVER_INDESKTOP 0x1")]
+        public const int APPIDREGFLAGS_ACTIVATE_IUSERVER_INDESKTOP = 0x1;
+
+        [NativeTypeName("#define APPIDREGFLAGS_SECURE_SERVER_PROCESS_SD_AND_BIND 0x2")]
+        public const int APPIDREGFLAGS_SECURE_SERVER_PROCESS_SD_AND_BIND = 0x2;
+
+        [NativeTypeName("#define APPIDREGFLAGS_ISSUE_ACTIVATION_RPC_AT_IDENTIFY 0x4")]
+        public const int APPIDREGFLAGS_ISSUE_ACTIVATION_RPC_AT_IDENTIFY = 0x4;
+
+        [NativeTypeName("#define APPIDREGFLAGS_IUSERVER_UNMODIFIED_LOGON_TOKEN 0x8")]
+        public const int APPIDREGFLAGS_IUSERVER_UNMODIFIED_LOGON_TOKEN = 0x8;
+
+        [NativeTypeName("#define APPIDREGFLAGS_IUSERVER_SELF_SID_IN_LAUNCH_PERMISSION 0x10")]
+        public const int APPIDREGFLAGS_IUSERVER_SELF_SID_IN_LAUNCH_PERMISSION = 0x10;
+
+        [NativeTypeName("#define APPIDREGFLAGS_IUSERVER_ACTIVATE_IN_CLIENT_SESSION_ONLY 0x20")]
+        public const int APPIDREGFLAGS_IUSERVER_ACTIVATE_IN_CLIENT_SESSION_ONLY = 0x20;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED1 0x40")]
+        public const int APPIDREGFLAGS_RESERVED1 = 0x40;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED2 0x80")]
+        public const int APPIDREGFLAGS_RESERVED2 = 0x80;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED3 0x100")]
+        public const int APPIDREGFLAGS_RESERVED3 = 0x100;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED4 0x200")]
+        public const int APPIDREGFLAGS_RESERVED4 = 0x200;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED5 0x400")]
+        public const int APPIDREGFLAGS_RESERVED5 = 0x400;
+
+        [NativeTypeName("#define APPIDREGFLAGS_AAA_NO_IMPLICIT_ACTIVATE_AS_IU 0x800")]
+        public const int APPIDREGFLAGS_AAA_NO_IMPLICIT_ACTIVATE_AS_IU = 0x800;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED7 0x1000")]
+        public const int APPIDREGFLAGS_RESERVED7 = 0x1000;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED8 0x2000")]
+        public const int APPIDREGFLAGS_RESERVED8 = 0x2000;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED9 0x4000")]
+        public const int APPIDREGFLAGS_RESERVED9 = 0x4000;
+
+        [NativeTypeName("#define APPIDREGFLAGS_RESERVED10 0x8000")]
+        public const int APPIDREGFLAGS_RESERVED10 = 0x8000;
+
+        [NativeTypeName("#define DCOMSCM_ACTIVATION_USE_ALL_AUTHNSERVICES 0x1")]
+        public const int DCOMSCM_ACTIVATION_USE_ALL_AUTHNSERVICES = 0x1;
+
+        [NativeTypeName("#define DCOMSCM_ACTIVATION_DISALLOW_UNSECURE_CALL 0x2")]
+        public const int DCOMSCM_ACTIVATION_DISALLOW_UNSECURE_CALL = 0x2;
+
+        [NativeTypeName("#define DCOMSCM_RESOLVE_USE_ALL_AUTHNSERVICES 0x4")]
+        public const int DCOMSCM_RESOLVE_USE_ALL_AUTHNSERVICES = 0x4;
+
+        [NativeTypeName("#define DCOMSCM_RESOLVE_DISALLOW_UNSECURE_CALL 0x8")]
+        public const int DCOMSCM_RESOLVE_DISALLOW_UNSECURE_CALL = 0x8;
+
+        [NativeTypeName("#define DCOMSCM_PING_USE_MID_AUTHNSERVICE 0x10")]
+        public const int DCOMSCM_PING_USE_MID_AUTHNSERVICE = 0x10;
+
+        [NativeTypeName("#define DCOMSCM_PING_DISALLOW_UNSECURE_CALL 0x20")]
+        public const int DCOMSCM_PING_DISALLOW_UNSECURE_CALL = 0x20;
+
+        [NativeTypeName("#define CLSCTX_VALID_MASK (CLSCTX_INPROC_SERVER | \\\r\n    CLSCTX_INPROC_HANDLER | \\\r\n    CLSCTX_LOCAL_SERVER | \\\r\n    CLSCTX_INPROC_SERVER16 | \\\r\n    CLSCTX_REMOTE_SERVER | \\\r\n    CLSCTX_NO_CODE_DOWNLOAD | \\\r\n    CLSCTX_NO_CUSTOM_MARSHAL | \\\r\n    CLSCTX_ENABLE_CODE_DOWNLOAD | \\\r\n    CLSCTX_NO_FAILURE_LOG | \\\r\n    CLSCTX_DISABLE_AAA | \\\r\n    CLSCTX_ENABLE_AAA | \\\r\n    CLSCTX_FROM_DEFAULT_CONTEXT | \\\r\n    CLSCTX_ACTIVATE_X86_SERVER | \\\r\n    CLSCTX_ACTIVATE_64_BIT_SERVER | \\\r\n    CLSCTX_ENABLE_CLOAKING | \\\r\n    CLSCTX_APPCONTAINER | \\\r\n    CLSCTX_ACTIVATE_AAA_AS_IU | \\\r\n    CLSCTX_RESERVED6 | \\\r\n    CLSCTX_ACTIVATE_ARM32_SERVER | \\\r\n    CLSCTX_ALLOW_LOWER_TRUST_REGISTRATION | \\\r\n    CLSCTX_PS_DLL)")]
+        public const int CLSCTX_VALID_MASK = ((int)(CLSCTX.CLSCTX_INPROC_SERVER) | (int)(CLSCTX.CLSCTX_INPROC_HANDLER) | (int)(CLSCTX.CLSCTX_LOCAL_SERVER) | (int)(CLSCTX.CLSCTX_INPROC_SERVER16) | (int)(CLSCTX.CLSCTX_REMOTE_SERVER) | (int)(CLSCTX.CLSCTX_NO_CODE_DOWNLOAD) | (int)(CLSCTX.CLSCTX_NO_CUSTOM_MARSHAL) | (int)(CLSCTX.CLSCTX_ENABLE_CODE_DOWNLOAD) | (int)(CLSCTX.CLSCTX_NO_FAILURE_LOG) | (int)(CLSCTX.CLSCTX_DISABLE_AAA) | (int)(CLSCTX.CLSCTX_ENABLE_AAA) | (int)(CLSCTX.CLSCTX_FROM_DEFAULT_CONTEXT) | (int)(CLSCTX.CLSCTX_ACTIVATE_X86_SERVER) | (int)(CLSCTX.CLSCTX_ACTIVATE_64_BIT_SERVER) | (int)(CLSCTX.CLSCTX_ENABLE_CLOAKING) | (int)(CLSCTX.CLSCTX_APPCONTAINER) | (int)(CLSCTX.CLSCTX_ACTIVATE_AAA_AS_IU) | (int)(CLSCTX.CLSCTX_RESERVED6) | (int)(CLSCTX.CLSCTX_ACTIVATE_ARM32_SERVER) | (int)(CLSCTX.CLSCTX_ALLOW_LOWER_TRUST_REGISTRATION) | (int)(CLSCTX.CLSCTX_PS_DLL));
+
+        [NativeTypeName("#define ROTFLAGS_REGISTRATIONKEEPSALIVE 0x1")]
+        public const int ROTFLAGS_REGISTRATIONKEEPSALIVE = 0x1;
+
+        [NativeTypeName("#define ROTFLAGS_ALLOWANYCLIENT 0x2")]
+        public const int ROTFLAGS_ALLOWANYCLIENT = 0x2;
+
+        [NativeTypeName("#define ROT_COMPARE_MAX 2048")]
+        public const int ROT_COMPARE_MAX = 2048;
+
+        [NativeTypeName("#define WDT_INPROC_CALL ( 0x48746457 )")]
+        public const int WDT_INPROC_CALL = (0x48746457);
+
+        [NativeTypeName("#define WDT_REMOTE_CALL ( 0x52746457 )")]
+        public const int WDT_REMOTE_CALL = (0x52746457);
+
+        [NativeTypeName("#define WDT_INPROC64_CALL ( 0x50746457 )")]
+        public const int WDT_INPROC64_CALL = (0x50746457);
+
+        [NativeTypeName("#define DECIMAL_NEG ((BYTE)0x80)")]
+        public const byte DECIMAL_NEG = ((byte)(0x80));
+
+        [NativeTypeName("#define VARIANT_TRUE ((VARIANT_BOOL)-1)")]
+        public const short VARIANT_TRUE = ((short)(-1));
+
+        [NativeTypeName("#define VARIANT_FALSE ((VARIANT_BOOL)0)")]
+        public const short VARIANT_FALSE = ((short)(0));
+    }
 }

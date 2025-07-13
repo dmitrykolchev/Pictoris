034381bb0e8aca088afadde7785950752e964cb8
Dmitry Kolchev <dmitrykolchev@msn.com>
2025-07-13 16:42:17 +0300

    add Base,rsp

---
+++ Managed.Win32/generation/Direct2d.c
@@ -0,0 +1,51 @@
+#include <Windows.h>
+
+#include <sdkddkver.h>
+
+#include <winerror.h>
+#include <dcommon.h>
+#include <d3dcommon.h>
+#include <dxgicommon.h>
+#include <dxgiformat.h>
+#include <dxgitype.h>
+#include <d2dbasetypes.h>
+#include <d2d1.h>
+#include <dciddi.h>
+#include <d2d1effects.h>
+#include <d2d1_1.h>
+#include <d2d1effectauthor.h>
+#include <d2d1effects_2.h>
+#include <d2d1_3.h>
+#include <d2d1_2.h>
+#include <d2d1svg.h>
+#include <d2d1effects_1.h>
+#include <d2d1effectauthor_1.h>
+#include <d2derr.h>
+#include <d2d1.h>
+#include <d2d1_1.h>
+#include <d2d1_1helper.h>
+#include <d2d1_2.h>
+#include <d2d1_2helper.h>
+#include <d2d1_3.h>
+#include <d2d1_3helper.h>
+
+#include <dwrite.h>
+#include <dwrite_1.h>
+#include <dwrite_2.h>
+#include <dwrite_3.h>
+
+#include <wincodec.h>
+#include <wincodecsdk.h>
+
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <dxgi1_3.h>
+#include <dxgi1_4.h>
+#include <dxgi1_5.h>
+#include <dxgi1_6.h>
+
+#include <d3dcommon.h>
+
+#include <documenttarget.h>
+
+#include <d3d11.h>
